a35ac8777748f4271d1da2d089707be2f49f0032
Higor Fernandes <higorofernandes@gmail.com>
2021-06-29 09:21:27 -0300

    Exercício 22 concluído

--- ex022.py
+++ ex022.py
@@ -1,11 +1,12 @@
 # Exercício Python 022: Crie um programa que leia o nome completo de uma pessoa e mostre: 
 # O nome com todas as letras maiúsculas e minúsculas.
 # Quantas letras ao todo (sem considerar espaços).
-nome=str(input('Digite seu nome completo: '))
-print(nome.upper())
-print(nome.lower())
-nome2=(nome.split())
-nome3=(''.join(nome2))
-nome4=nome3.count('')
-print(f'O nome {nome} tem {nome4} caracteres')
+# Quantas letras tem o primeiro nome?
+nome=str(input('Digite seu nome completo: ')).strip()
+print(f'O seu nome maisúculo é: {nome.upper()}')
+print(f'O seu nome minúsculo é: {nome.lower()}')
+print('O nome {} tem ao todo {} letras'.format(nome,len(nome)-nome.count(' ')))
+nome2=nome.split()
+# print('O nome {} tem {} letras'.format(nome2[0], nome.find(' ')))
+print('O nome {} tem {} letras'.format(nome2[0], len(nome2[0])))
 
